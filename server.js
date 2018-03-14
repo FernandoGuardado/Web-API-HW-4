fca9e5379b2d73936da76126daf279054fb892fa
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-03-14 16:02:18 -0600

    a

--- server.js
+++ server.js
@@ -79,18 +79,11 @@ router.post('/signup', function(req, res) {
             }
             else {
             var user = new User();
-            //user.name = req.body.name;
+            user.name = req.body.name;
             user.username = req.body.username;
             user.password = req.body.password;
             // save the user
             user.save(function(err) {
-                      if (err) {
-                      // duplicate entry
-                      if (err.code == 11000)
-                      return res.json({ success: false, message: 'A user with that username already exists. '});
-                      else
-                      return res.send(err);
-                      }
                       
                       res.json({ message: 'User created!' });
                       });
