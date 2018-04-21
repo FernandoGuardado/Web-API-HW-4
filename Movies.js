aa8c9f51ad7a77d11d139c7f403180431b1d5694
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-04-21 02:25:59 -0600

    A

--- Movies.js
+++ Movies.js
@@ -2,18 +2,18 @@
 // Movie Schema Jason File for Mongoose 
 
 var mongoose = require('mongoose');
-var schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
-// movie schema
+//Movie schema
 var MovieSchema = new Schema({
-                             title: { type: String, required: true, index: { unique: true }},
-                             year: { type: String, required: true },
-                             genre: { type: String, required: true, enum:['Action','Adventure','Comedy','Drama','Fantasy','Horror','Mystery','Thriller','Western']},
-                             actors : { type : Array , "default" : [] }
-                             });
+    title: { type: String, required: true, index: { unique: true }},
+    year: { type: String, required: true },
+    genre: { type: String, required: true, enum:['Action','Adventure','Comedy','Drama','Fantasy','Horror','Mystery','Thriller','Western']},
+    actors : { type : Array , "default" : [] }
+});
 
 
-// return movie
-module.exports = mongoose.model('Movie:', MovieSchema);
+//return the model
+module.exports = mongoose.model('Movie', MovieSchema);
