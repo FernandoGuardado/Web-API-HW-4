a867c91b32a0f28bac0e1a1923377eb45eccb103
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-04-21 05:33:53 -0600

    a

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ router.route('/movies')
         Movie.findByIdAndRemove(req.body._id,function (err, movie) {
             if (err) res.send(err);
                                 
-            res.json({ message: 'Movie has been deleted from the database...' + req.body.title});
+            res.json({ message: req.body.title + 'has been deleted from the database.'});
             });
         });
 //===============================================================================================
